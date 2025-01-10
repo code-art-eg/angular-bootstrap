222e5518144ae3f1e5fcc5c0701de5726776a3fa
sherif-elmetainy <sherif.elmetainy@gmail.com>
2025-01-10 14:37:41 +0200

    add library project

---
+++ projects/angular-bootstrap/src/lib/angular-bootstrap.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-bootstrap',
+  imports: [],
+  template: `
+    <p>
+      angular-bootstrap works!
+    </p>
+  `,
+  styles: ``
+})
+export class AngularBootstrapComponent {
+
+}
---
+++ projects/angular-bootstrap/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-bootstrap
+ */
+
+export * from './lib/angular-bootstrap.service';
+export * from './lib/angular-bootstrap.component';
